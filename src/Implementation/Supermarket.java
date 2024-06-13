fdd9284aec17aa16d88e9c356981567c01ae3d39
renatoryu <renatoryuu@hotmail.com>
2024-06-12 23:50:25 -0300

    Supermarket

---
+++ src/Implementation/Supermarket.java
@@ -0,0 +1,9 @@
+package Implementation;
+
+public interface Supermarket {
+    void add(String item);
+
+    void print();
+
+    void delete(int index);
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,46 @@
+package Main;
+import java.util.Scanner;
+
+import Implementation.Supermarket;
+import Implementation.SupermarketArray;
+
+public class Main {
+    private static int SIZE = 3;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Supermarket supermarket = new SupermarketArray(SIZE);
+        int opcao;
+
+        do {
+            System.out.println("\n Lista de Compras");
+            System.out.println("1 - Inserir");
+            System.out.println("2 - Listar");
+            System.out.println("3 - Remover");
+            System.out.println("4 - Sair");
+            System.out.println("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o item a ser inserido: ");
+                    String item = scanner.next();
+                    supermarket.add(item);
+                    break;
+                case 2:
+                    supermarket.print();
+                    break;
+                case 3:
+                    System.out.println("Digite a posição do item a ser removido: ");
+                    int index = scanner.nextInt();
+                    supermarket.delete(index);
+                case 4:
+                    System.out.println("Saindo do programa...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Por favor, escolha novamente.");
+            }
+        } while (opcao != 4);
+        scanner.close();
+    }
+}
